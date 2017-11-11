7102ed1b3309b042419f2a1649043d6e86f7f3c0
Santiago Garza <santiago.garza@imforza.com>
2017-11-11 15:34:34 -0800

    Rewrite started

--- wp-connectwise-api.php
+++ wp-connectwise-api.php
@@ -28,6 +28,15 @@ if ( ! class_exists( 'ConnectWiseAPI' ) ) {
 	 */
 	class ConnectWiseAPI {
 
+
+
+		/**
+		 * Route being called.
+		 *
+		 * @var string
+		 */
+		protected $route = '';
+
 		/**
 		 * ConnectWise Site
 		 *
@@ -73,37 +82,94 @@ if ( ! class_exists( 'ConnectWiseAPI' ) ) {
 
 			static::$connectwise_site = $connectwise_site;
 			static::$connectwise_version = $connectwise_version;
+			static::$company_id = $company_id;
+			static::$public_key = $public_key;
+			static::$connectwise_version = $private_key;
 
-			$this->base_uri = 'https://' . $connectwise_site . '/' . static::$connectwise_version;
-
-			$this->args['headers'] = array(
-				'Content-Type' => 'application/json',
-				'Authorization' => 'Basic ' . base64_encode($company_id.'+'.$public_key.':'.$private_key),
-				// 'Accept' => 'application/vnd.connectwise.com+json; version=3.0.0',
-			);
-
+			$this->base_uri = 'https://' . $connectwise_site . '/' . static::$connectwise_version . '/apis/3.0/';
 		}
+
+		/**
+		 * Prepares API request.
+		 *
+		 * @param  string $route   API route to make the call to.
+		 * @param  array  $args    Arguments to pass into the API call.
+		 * @param  array  $method  HTTP Method to use for request.
+		 * @return self            Returns an instance of itself so it can be chained to the fetch method.
+		 */
+		protected function build_request( $route, $args = array(), $method = 'GET' ) {
+			// Headers get added first.
+			$this->set_headers();
+
+			// Add Method and Route.
+			$this->args['method'] = $method;
+			$this->route = $route;
+
+			// Generate query string for GET requests.
+			if ( 'GET' === $method ) {
+				$this->route = add_query_arg( array_filter( $args ), $route );
+			}
+			// Add to body for all other requests. (Json encode if content-type is json).
+			elseif ( 'application/json' === $this->args['headers']['Content-Type'] ) {
+				$this->args['body'] = wp_json_encode( $args );
+			} else {
+				$this->args['body'] = $args;
+			}
+
+			return $this;
+		}
+
 
 		/**
 		 * Fetch the request from the API.
 		 *
 		 * @access private
-		 * @param mixed $request Request URL.
-		 * @return $body Body.
+		 * @return array|WP_Error Request results or WP_Error on request failure.
 		 */
-		private function fetch( $request ) {
+		protected function fetch() {
+			// Make the request.
+			$response = wp_remote_request( $this->base_uri . $this->route, $this->args );
 
-			$response = wp_remote_get( $request, $this->args );
+			// Retrieve Status code & body.
 			$code = wp_remote_retrieve_response_code( $response );
+			$body = json_decode( wp_remote_retrieve_body( $response ) );
 
-			if ( 200 !== $code ) {
-				return new WP_Error( 'response-error', sprintf( __( 'Server response code: %d', 'wp-connectwise-api' ), $code ) );
+			$this->clear();
+			// Return WP_Error if request is not successful.
+			if ( ! $this->is_status_ok( $code ) ) {
+				return new WP_Error( 'response-error', sprintf( __( 'Status: %d', 'wp-postmark-api' ), $code ), $body );
 			}
 
-			$body = wp_remote_retrieve_body( $response );
+			return $body;
+		}
 
-			return json_decode( $body );
 
+		/**
+		 * Set request headers.
+		 */
+		protected function set_headers() {
+			// Set request headers.
+			$this->args['headers'] = array(
+				'Content-Type' => 'application/json',
+				'Authorization' => 'Basic ' . base64_encode( static::$company_id . '+' . static::$public_key . ':' . static::$private_key ),
+			);
+		}
+
+		/**
+		 * Clear query data.
+		 */
+		protected function clear() {
+			$this->args = array();
+		}
+
+		/**
+		 * Check if HTTP status code is a success.
+		 *
+		 * @param  int $code HTTP status code.
+		 * @return boolean       True if status is within valid range.
+		 */
+		protected function is_status_ok( $code ) {
+			return ( 200 <= $code && 300 > $code );
 		}
 
 		/* COMPANIES. */
@@ -148,12 +214,28 @@ if ( ! class_exists( 'ConnectWiseAPI' ) ) {
 		 * @param mixed $page_size Page Size.
 		 * @return void
 		 */
-		public function get_companies( $conditions = '', $order_by = '', $child_conditions = '', $custom_field_conditions = '', $page = '', $page_size = '' ) {
+		public function get_companies( $conditions = null, $order_by = null, $child_conditions = null, $custom_field_conditions = null, $page = null, $page_size = null ) {
 
-			$request = $this->base_uri . '/apis/3.0/company/companies';
+			if ( null !== $conditions ) {
+				$args['conditions']  = $conditions;
+			}
+			if ( null !== $order_by ) {
+				$args['orderBy']  = $order_by;
+			}
+			if ( null !== $child_conditions ) {
+				$args['childconditions']  = $child_conditions;
+			}
+			if ( null !== $custom_field_conditions ) {
+				$args['customfieldconditions']  = $custom_field_conditions;
+			}
+			if ( null !== $page ) {
+				$args['page']  = $page;
+			}
+			if ( null !== $page_size ) {
+				$args['pageSize']  = $page_size;
+			}
 
-			return $this->fetch( $request );
-
+			return $this->build_request( 'company/companies', $args )->fetch();
 		}
 
 		public function create_company() {
